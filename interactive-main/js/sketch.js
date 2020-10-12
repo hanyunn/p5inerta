eb5b7b28ccb42b4eeda4c80073a6623dbc23089a
hanyunn <71659808+hanyunn@users.noreply.github.com>
2020-10-11 21:13:33 -0700

    Add files via upload

---
+++ interactive-main/js/sketch.js
@@ -0,0 +1,55 @@
+
+
+function setup() {
+  // put setup code here to run once
+
+  //creat canvas 500 by 500 px
+  createCanvas(windowWidth,windowHeight);
+
+  background(320, 180, 540);
+
+  print(windowWidth);
+}
+
+function draw() {
+
+  //change rectangle x,y coordinates to center
+  rectMode(CENTER);
+
+  //point is only x,y coordinate
+  strokeWeight(300);
+  point(mouseX, mouseY);
+
+  //styles for the ellipse
+  noStroke();
+  fill(0, 0, 0);
+  ellipse(850, 450, 600, 600);
+
+  noStroke();
+  fill(32, 18, 54);
+  ellipse(850, 450, 500, 500);
+
+  noStroke();
+  fill(60, 34, 102);
+  ellipse(850, 450, 400, 400);
+
+  noStroke();
+  fill(82, 46, 140);
+  ellipse(850, 450, 300, 300);
+
+  noStroke();
+  fill(106, 60, 181);
+  ellipse(850, 450, 200, 200);
+
+  noStroke();
+  fill(130, 73, 222);
+  ellipse(850, 450, 100, 100);
+
+
+  smooth();
+  //styles for the point
+  fill(120);
+  stroke(137, 112, 161)
+  print(mouseX);
+
+}
